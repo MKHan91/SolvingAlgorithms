0ef885b7a0442a0f6d6e05d7c049937e6e901037
MKHan91 <audrbz@naver.com>
2020-12-25 21:30:27 +0900

    delete

--- HackerRanks/Basic_Data_Types/Find_the_Runner_up_Score.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-
-    set_arr = list(set(arr))
-    set_arr.remove(max(arr))
-
-    print(max(set_arr))
-    a=1
