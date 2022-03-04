d29c4c8c2844cee6faa346483e3cb59804f7e69b
Myungkyu Han <35263213+MKHan91@users.noreply.github.com>
2022-03-04 23:13:40 +0900

    Delete blind_number.py

--- Programmers_ps/blind_number.py
+++
@@ -1,8 +0,0 @@
-def solution(phone_number):
-    # answer = "*" * len(phone_number[:-4]) + phone_number[len(phone_number[:-4]):]
-    answer = phone_number.replace(phone_number[:-4], "*" * len(phone_number[:-4]))
-    return answer
-
-if __name__ == "__main__":
-    phone_number = "01033334444"
-    print(solution(phone_number))
