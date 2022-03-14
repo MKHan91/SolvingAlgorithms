e237152ab7b018a1bc0d608a2cc3c7d066dd9e52
MKHan91 <audrbz@naver.com>
2022-03-14 22:51:29 +0900

    Solved

--- Programmers/Problem_Solving/greedy_ps.py
+++ Programmers/Problem_Solving/greedy_ps.py
@@ -3,29 +3,37 @@ def solution(n, lost, reserve):
     reserve = sorted(reserve)
     lost = sorted(lost)
     
-    intersection = set(lost) & set(reserve)
-    for inter in intersection:
-        reserve.remove(inter)
-        lost.remove(inter)
-    
-    print(lost)
-    print(reserve)
-    for student in reserve:
-        if student - 1 in lost:
+    for student in lost[:]:
+        if student in reserve:
+            reserve.remove(student)
+            lost.remove(student)
             answer += 1
-            lost.pop(lost.index(student-1))
+            continue
         
-        elif student + 1 in lost:
+        if student - 1 in reserve:
+            if student in reserve:
+                continue
             answer += 1
-            lost.pop(lost.index(student+1))
-    
+            reserve.remove(student-1)
+        
+        elif student + 1 in reserve:
+            if student + 1 in lost:
+                continue
+            else:
+                answer += 1
+                reserve.remove(student+1)
+            
     return answer
+    
 
 if __name__ == "__main__":
     # n, lost, reserve = 5, [1, 2, 3, 4, 5], [1, 2, 3]
     # n, lost, reserve = 3, [1, 2], [2, 3]
-    n, lost, reserve = 20, [4, 10, 6, 13, 14, 9], [3, 5, 8, 11, 12]
+    # n, lost, reserve = 3, [1, 2], [1, 3, 2]
+    # n, lost, reserve = 20, [4, 10, 6, 13, 14, 9], [3, 5, 8, 11, 12]
     # n, lost, reserve = 20, [1], [1, 2]
     # n, lost, reserve = 5, [4, 3], [3, 2]
+    n, lost, reserve = 5, [4, 3], [2, 3]
+    
     # n, lost, reserve = 3, [3], [1]
     print(solution(n, lost, reserve))
