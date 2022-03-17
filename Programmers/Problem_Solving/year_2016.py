f45810daaf477c814bd86c039f385ff906270c36
MKHan91 <audrbz@naver.com>
2022-03-17 21:13:42 +0900

    Solving

--- Programmers/Problem_Solving/year_2016.py
+++ Programmers/Problem_Solving/year_2016.py
@@ -1,16 +1,14 @@
 def solution(a, b):
     answer = ''
-    week = ["MON", "TUE", "WED", "THU", "FRI", "SAT", 'SUN']
-    full = [1, 3, 5, 7, 8, 10, 12]
-    not_full = [2, 4, 6, 9, 11]
-    
-    for month in range(1, 13):
-        for day in range(1, 32):
-            if month in full:
-                print(week[(31 + 31 + 29 + 31 + 30)%7])
-        a=1
+    weekend = ["FRI", "SAT", 'SUN', "MON", "TUE", "WED", "THU"]
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if a == 1:
+        answer = weekend[(b-1)%7]
+    else:
+        answer = weekend[(sum(month[:a-1]) + ((b-1) % 7))%7]
     return answer
 
 if __name__ == "__main__":
     a, b = 5, 24
+    # a, b = 3, 15
     print(solution(a, b))
