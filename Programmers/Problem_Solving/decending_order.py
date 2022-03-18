15b094e6ffaff8ce4661f8e6ee052ab5889ea24e
MKHan91 <audrbz@naver.com>
2022-03-18 21:30:21 +0900

    solving

--- Programmers/Problem_Solving/decending_order.py
+++ Programmers/Problem_Solving/decending_order.py
@@ -1,8 +1,6 @@
 def solution(n):
-    answer = 0
-    answer = list(n)
+    answer = int("".join(sorted(str(int(n)), reverse=True)))
     return answer
 
 if __name__ == "__main__":
-    n = 118372
     print(solution(n))
