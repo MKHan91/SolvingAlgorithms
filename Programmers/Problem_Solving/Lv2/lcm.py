df1dd908b514bb617c24ebe87d1cb454394df9d3
MKHan91 <audrbz@naver.com>
2022-03-31 21:52:47 +0900

    not solving

---
+++ Programmers/Problem_Solving/Lv2/lcm.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = 0
+    a, b = min(arr), max(arr)
+    t=14
+    while t > 0:
+        a=1
+    return answer
+
+if __name__ == "__main__":
+    arr = [2,6,8,14]
+    print(solution(arr))
