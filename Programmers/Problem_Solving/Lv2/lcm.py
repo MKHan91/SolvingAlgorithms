0cf739a137e4ec44cc67414dae7661477a477c26
MKHan91 <audrbz@naver.com>
2022-04-06 22:39:44 +0900

    solving

--- Programmers/Problem_Solving/Lv2/lcm.py
+++ Programmers/Problem_Solving/Lv2/lcm.py
@@ -1,32 +1,30 @@
-def solution(arr):
-    answer = 1
-    sorted_arr = sorted(arr)
-    
-    idx = 0
-    min_value = sorted_arr[0]
-    queue = []
-    while idx != len(sorted_arr)-1:
-        a = sorted_arr[idx+1]
-        
-        remain = a % min_value
-        if remain == 0:
-            answer *= (a // min_value)
-            idx += 1
-        else:
-            queue.append(a)
-            idx += 1
-            if idx == len(sorted_arr)-1:
-                answer *= min_value
-                min_value = queue.pop(0)
-                idx -= 1
+# 재귀 함수를 이용한 gcd 구하기. 
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
 
-    if len(queue) != 0:
-        answer *= queue.pop(0)
-    answer *= min_value
+def lcm(a, b):
+    max_value, min_value = max(a, b), min(a, b)
+    remain = 1
+    while remain > 0:
+        remain = max_value % min_value
+        max_value, min_value = min_value, remain
+    
+    lcm = a*b//max_value
+    return lcm 
+
+def solution(arr):
+    arr = sorted(arr)
+
+    while len(arr) >= 2:
+        a, b = arr.pop(0), arr.pop(0)
+        arr.insert(0, lcm(a, b))
+    answer = arr[0]
     return answer
 
 if __name__ == "__main__":
     arr = [2,6,7,14]
-    # arr = [2,6,7,8]
+    # arr = [2,6,8,14]
     # arr = [1, 2, 3]
     print(solution(arr))
