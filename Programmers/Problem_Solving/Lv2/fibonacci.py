4ad51c29b9cf2eebb474c25c32306bd5b6f50efb
MKHan91 <audrbz@naver.com>
2022-04-10 23:41:37 +0900

    Recommending the iterative solution

--- Programmers/Problem_Solving/Lv2/fibonacci.py
+++ Programmers/Problem_Solving/Lv2/fibonacci.py
@@ -1,11 +1,27 @@
 def solution(n):
+    # 재귀 함수를 쓸 경우 iterative 방법보다 더 많은 시간 복잡도 소요
     f0, f1 = 0, 1
-    if n == 2:
+    if n == 0:
+        answer = f0
+    elif n <= 2:
         answer = f0 + f1
+    elif n > 2:
+        answer = solution(n-2) + solution(n-1)
+        answer %= 1234567
+    return answer
+
+def iterative_solution(n):
+    fibonacci_series = [0, 1]
+    if n <= 1:
+        answer = n
     else:
-        answer = 1 + solution(n-1)
+        for i in range(1, n):
+            value = fibonacci_series[i-1] + fibonacci_series[i]
+            fibonacci_series.append(value)
+    answer = fibonacci_series[-1] % 1234567
     return answer
 
 if __name__ == "__main__":
     n = 4
-    print(solution(n))
+    # print(solution(n))
+    print(iterative_solution(n))
